04b3a595bfcd9db300de572d4f2e7fa82ec214b0
Artur Manukian <manukian.artur.94@gmail.com>
2019-07-11 17:44:47 +0300

    Fix return validators.

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -1,27 +1,19 @@
 import { body, param } from 'express-validator';
 
-const EmailValidator = () => [
-  body('email', 'Should be an email')
-    .isEmail(),
-];
+const EmailValidator = () => body('email', 'Should be an email')
+  .isEmail();
 
-const PasswordValidator = () => [
-  body('password', 'Length should be not less 5 symbols')
-    .trim()
-    .isLength({ min: 5 }),
-];
+const PasswordValidator = () => body('password', 'Length should be not less 5 symbols')
+  .trim()
+  .isLength({ min: 5 });
 
-const MongoIdValidator = (field) => [
-  param(field, 'Invalid ID type')
-    .isMongoId(),
-];
+const MongoIdValidator = (field) => param(field, 'Invalid ID type')
+  .isMongoId();
 
-const MessageBodyValidator = () => [
-  body('messageBody', 'Message should not be empty')
-    .not()
-    .isEmpty()
-    .trim(),
-];
+const MessageBodyValidator = () => body('messageBody', 'Message should not be empty')
+  .not()
+  .isEmpty()
+  .trim();
 
 export {
   EmailValidator,
