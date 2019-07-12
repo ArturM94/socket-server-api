6cb0f009bb4df5c274bacd7a002c31aebecfbfeb
Artur Manukian <manukian.artur.94@gmail.com>
2019-07-12 10:56:06 +0300

    Add unit tests for reset password.

--- src/tests/auth-service.test.js
+++ src/tests/auth-service.test.js
@@ -1,6 +1,6 @@
 import chai, { assert, expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import mongoose from 'mongoose';
+import mongoose, { CastError } from 'mongoose';
 import * as argon2 from 'argon2';
 import { connectToDatabase } from '../database';
 import config from '../config';
@@ -12,7 +12,10 @@ chai.use(chaiAsPromised);
 let mockUser;
 let email;
 let password;
+let newPassword;
 let hashedPassword;
+let invalidUserId;
+let invalidUserPassword;
 
 connectToDatabase(config.db.TEST);
 
@@ -21,7 +24,12 @@ setup(async () => {
 
   email = 'testuser@gmail.com';
   password = 'password';
+  newPassword = 'newPassword';
   hashedPassword = await argon2.hash(password);
+
+  const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+  invalidUserId = randomNumber();
+  invalidUserPassword = randomNumber();
 });
 
 suite('Auth Service', () => {
@@ -46,6 +54,19 @@ suite('Auth Service', () => {
       assert.equal(mockUser.email, user.email, 'mock-user email equal to putted email');
       assert.equal(generatedToken, token, 'mock-user token equal to generated token');
     });
+
+    test('#resetUserPassword()', async () => {
+      mockUser = await User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      const { user } = await AuthService.login(mockUser.email, password);
+      await AuthService.resetUserPassword(user._id, newPassword);
+      const { user: userWithNewPassword } = await AuthService.login(mockUser.email, newPassword);
+
+      assert.equal(user.email, userWithNewPassword.email, 'user email with new password equal to user email');
+    });
   });
 
   suite('negative tests', () => {
@@ -69,6 +90,32 @@ suite('Auth Service', () => {
 
       return expect(loginWithIncorrectPassword).to.eventually
         .be.rejectedWith(Error, 'Password is incorrect.', 'login with incorrect password');
+    });
+
+    test('#resetUserPassword() - invalid user id', async () => {
+      mockUser = await User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      const resetPassword = AuthService.resetUserPassword(invalidUserId, newPassword);
+
+      return expect(resetPassword).to.eventually
+        .be.rejectedWith(CastError, null, 'get CastError due invalid user id')
+        .and.be.an.instanceOf(Error);
+    });
+
+    test('#resetUserPassword() - invalid password', async () => {
+      mockUser = await User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      const resetPassword = AuthService.resetUserPassword(mockUser._id, invalidUserPassword);
+
+      return expect(resetPassword).to.eventually
+        .be.rejectedWith(TypeError, null, 'get TypeError due invalid password data type')
+        .and.be.an.instanceOf(Error);
     });
   });
 });
